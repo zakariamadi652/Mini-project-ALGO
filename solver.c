a4c7c650a80b41e5ed534e039f00d5f851a8c4fe
Racimld06 <racimlaidi1@gmail.com>
2025-11-21 18:20:21 +0100

    Add files via upload

---
+++ solver.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define WORD_LENGTH 5
+#define MAX_WORDS 2315
+void load_all_words(char words[][WORD_LENGTH + 1], int *word_count) {
+    FILE *file = fopen("data.txt", "r");
+    if (file == NULL) {
+        printf("rror opening data.txt!\n");
+        *word_count = 0;
+        return;
+    }
+    
+    *word_count = 0;
+    char word[WORD_LENGTH + 2];
+    
+    while (fscanf(file, "%5s", word) == 1 && *word_count < MAX_WORDS) {
+        strcpy(words[*word_count], word);
+        (*word_count)++;
+    }
+    
+    fclose(file);
+    printf("dictionary loaded: %d words\n", *word_count);
+}
