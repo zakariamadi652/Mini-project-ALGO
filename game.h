59dfa4e88fac25b472f0bf3dad2bbbbda405e92b
Racimld06 <racimlaidi1@gmail.com>
2025-11-22 10:53:27 +0100

    Delete game.h

--- game.h
+++
@@ -1,9 +0,0 @@
-#ifndef GAME_H
-#define GAME_H
-
-void select_random_word(char random_word[]);
-void comparing_letter_by_letter(char random_word[], char word_from_the_user[]);
-void play_wordle();
-void auto_solve();
-
-#endif
