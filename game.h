b02ac1efd3cb418d70fad227051f8be64ce5f3e6
Racimld06 <racimlaidi1@gmail.com>
2025-11-22 10:15:23 +0100

    Add files via upload

---
+++ game.h
@@ -0,0 +1,9 @@
+#ifndef GAME_H
+#define GAME_H
+
+void select_random_word(char random_word[]);
+void comparing_letter_by_letter(char random_word[], char word_from_the_user[]);
+void play_wordle();
+void auto_solve();
+
+#endif
