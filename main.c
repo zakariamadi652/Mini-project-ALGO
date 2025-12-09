ceb9b5a3337be7dff29eb8fa02839d768b6d4991
Academicwpn <nasri.anes.zakaria.242439340418@gmail.com>
2025-12-09 23:26:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
 #include "wordle.h"
 
 int main() {
-    int choice;
     if (!load_words("data.txt")) {
-        printf("error could not load dictionary file 'data.txt'\n");
+        printf("Error loading dictionary.\n");
         return 1;
     }
-    srand(time(NULL));
-    printf("=== WORDLE GAME ===\n");
+
+    int option;
     printf("1. Play Wordle\n");
-    printf("2. Start solver\n");
-    printf("3. Exit\n");
-    printf("Choice: ");
-    scanf("%d", &choice);
-    switch(choice) {
-        case 1:
-            play_wordle();  
-            break;
-        case 2:
-            auto_solve();   
-            break;
-        case 3:
-            printf("Goodbye!!\n");
-            break;
-        default:
-            printf("invalide choice\n");
-    }
+    printf("2. Auto-solver\n");
+    printf("> ");
+    scanf("%d", &option);
+
+    if (option == 1) play_wordle();
+    else if (option == 2) auto_solve();
+
     return 0;
 }
