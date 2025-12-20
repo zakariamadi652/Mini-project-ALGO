23b2d7398429e89c666dd2a149181aa889c2aa1f
zakariamadi652 <madi.zakaria@etu.usthb.dz>
2025-12-20 23:30:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,10 @@ int main() {
     }
     
     int option;
-    printf("=== Wordle Solver Menu ===\n");
+    printf("=== Wordle Game Menu ===\n");
     printf("1. Play Wordle\n");
     printf("2. Auto-solver\n");
-    printf("3. auto solver 2\n");
+    printf("3. Auto-solver 2\n");
     printf(">");
     scanf("%d", &option);
     if (option == 1) play_wordle();
